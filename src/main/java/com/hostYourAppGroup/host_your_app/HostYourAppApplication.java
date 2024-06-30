6733f4dd21ec3866ec753fdb9c051764c47d248e
sai2021krishna <saikrishna.at2021@gmail.com>
2024-06-30 09:47:26 -0700

    HYA-12 | initial java code push

---
+++ src/main/java/com/hostYourAppGroup/host_your_app/HostYourAppApplication.java
@@ -0,0 +1,13 @@
+package com.hostYourAppGroup.host_your_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostYourAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostYourAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hostYourAppGroup/host_your_app/HostYourAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostYourAppGroup.host_your_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostYourAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
